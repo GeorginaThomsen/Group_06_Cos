f47baa721153a6363274e8d95eb0885db621b7b9
Georgina Thomsen <georgina-thomsen@hotmail.com>
2015-04-08 15:31:52 +0200

    Added RequestProject jsp, and made the project class (with an ugly toString):)

--- src/java/domain/Project.java
+++ src/java/domain/Project.java
@@ -5,10 +5,122 @@
  */
 package domain;
 
+import java.util.Date;
+
 /**
  *
  * @author Cookie
  */
 public class Project {
+    private int ProjectID;
+    private String ActivityDescription;
+    private String Comments;
+    private float Cost;
+    private String MDFBudget;
+    private String ExecutionQuarter;
+    private Date StartDate;//changed it to date instead of string. Either change uml or change this back.
+    private Date EndDate;
+    private String ObjAndResult;
+    private String RequiredPOE;
+
+    public Project(int ProjectID, String ActivityDescription, String Comments, float Cost, String MDFBudget, String ExecutionQuarter, Date StartDate, Date EndDate, String ObjAndResult, String RequiredPOE) {
+        this.ProjectID = ProjectID;
+        this.ActivityDescription = ActivityDescription;
+        this.Comments = Comments;
+        this.Cost = Cost;
+        this.MDFBudget = MDFBudget;
+        this.ExecutionQuarter = ExecutionQuarter;
+        this.StartDate = StartDate;
+        this.EndDate = EndDate;
+        this.ObjAndResult = ObjAndResult;
+        this.RequiredPOE = RequiredPOE;
+    }
+
+    public int getProjectID() {
+        return ProjectID;
+    }
+
+    public void setProjectID(int ProjectID) {
+        this.ProjectID = ProjectID;
+    }
+
+    public String getActivityDescription() {
+        return ActivityDescription;
+    }
+
+    public void setActivityDescription(String ActivityDescription) {
+        this.ActivityDescription = ActivityDescription;
+    }
+
+    public String getComments() {
+        return Comments;
+    }
+
+    public void setComments(String Comments) {
+        this.Comments = Comments;
+    }
+
+    public float getCost() {
+        return Cost;
+    }
+
+    public void setCost(float Cost) {
+        this.Cost = Cost;
+    }
+
+    public String getMDFBudget() {
+        return MDFBudget;
+    }
+
+    public void setMDFBudget(String MDFBudget) {
+        this.MDFBudget = MDFBudget;
+    }
+
+    public String getExecutionQuarter() {
+        return ExecutionQuarter;
+    }
+
+    public void setExecutionQuarter(String ExecutionQuarter) {
+        this.ExecutionQuarter = ExecutionQuarter;
+    }
+
+    public Date getStartDate() {
+        return StartDate;
+    }
+
+    public void setStartDate(Date StartDate) {
+        this.StartDate = StartDate;
+    }
+
+    public Date getEndDate() {
+        return EndDate;
+    }
+
+    public void setEndDate(Date EndDate) {
+        this.EndDate = EndDate;
+    }
+
+    public String getObjAndResult() {
+        return ObjAndResult;
+    }
+
+    public void setObjAndResult(String ObjAndResult) {
+        this.ObjAndResult = ObjAndResult;
+    }
+
+    public String getRequiredPOE() {
+        return RequiredPOE;
+    }
+
+    public void setRequiredPOE(String RequiredPOE) {
+        this.RequiredPOE = RequiredPOE;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" + "ProjectID=" + ProjectID + ", ActivityDescription=" + ActivityDescription + ", Comments=" + Comments + ", Cost=" + Cost + ", MDFBudget=" + MDFBudget + ", ExecutionQuarter=" + ExecutionQuarter + ", StartDate=" + StartDate + ", EndDate=" + EndDate + ", ObjAndResult=" + ObjAndResult + ", RequiredPOE=" + RequiredPOE + '}';
+    }
+    
+    
     
 }
---
+++ web/RequestProject.jsp
@@ -0,0 +1,31 @@
+<%-- 
+    Document   : RequestProject
+    Created on : 08-04-2015, 14:23:33
+    Author     : Cookie
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Partner MDF Tool</title>
+    </head>
+    <body>
+        <h1>Request Project</h1>
+        <form action="">
+            Activity Description:<br><input type="text" name="ActivityDescription"><br>
+            Comments:<br><input type="text" name="Comments"><br>
+            Cost:<br><input type="text" name="Cost"><br>
+            MDF Budget:<br><input type="text" name="MDFBudget"><br>
+            Execution Quarter:<br><input type="text" name="ExecutionQuarter"><br>
+            Start Date:<br><input type="text" name="StartDate"><br>
+            End Date:<br><input type="text" name="EndDate"><br>
+            Objective & Result:<br><input type="text" name="ObjAndResult"><br>
+            Required POE:<br><input type="text" name="RequiredPOE"><br>
+            <input type="hidden" name="command" value="RequestProject">
+            <br><button name="RequestProjectButton" value="" >Request Project</Button>
+
+        </form>
+    </body>
+</html>
